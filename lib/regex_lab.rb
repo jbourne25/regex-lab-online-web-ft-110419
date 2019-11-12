6645ed7d0bddb1c0eee4e46e9b610047176539ec
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-12 03:23:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,11 @@ end
 
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  
+   if text.match(/^[A-Z].+[\.!?]$/) then
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
