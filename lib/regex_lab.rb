2ae8c45fe5a2b1321f925ea5b35fcd3a713c8130
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-12 03:19:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,8 +7,8 @@ def starts_with_a_vowel?(word)
 end	
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/^[un][$]+\w/)
-end
+   text.scan(/un+\w+ing\b/)
+end	
 
 def words_five_letters_long(text)
 
